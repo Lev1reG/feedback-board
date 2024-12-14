1677f27f23bec52a9ef61371db557f7988a8543c
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-12-14 12:31:02 +0700

    feat: structure & styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+import { twJoin } from "tailwind-merge";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main
+      className={twJoin(
+        "md:max-w-2xl",
+        "mx-auto md:mt-8",
+        "bg-white",
+        "md:shadow-lg",
+        "md:rounded-lg",
+        "overflow-hidden",
+      )}
+    >
+      <div
+        className={twJoin("p-8", "bg-gradient-to-r from-cyan-400 to-blue-400")}
+      >
+        <h1 className="font-bold text-xl">Feedback Platform by Deren</h1>
+        <p className="text-opacity-90 text-slate-700">
+          Collaborative platform for gathering, discussing, and sharing ideas.
+        </p>
+      </div>
+      <div className={twJoin("flex", "px-8 py-2", "bg-gray-100", "border-b")}>
+        <div className={twJoin("grow")}></div>
+        <div>
+          <button
+            className={twJoin(
+              "px-4 py-1",
+              "bg-blue-500",
+              "rounded-md",
+              "text-white text-opacity-90",
+            )}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Make a suggestion
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className={twJoin("px-8 py-4")}>
+        <div className={twJoin("flex", "items-center", "gap-8")}>
+          <div>
+            <h2>Please Post more videos and post more often</h2>
+            <p className="text-gray-600 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              dignissim leo sit amet sem egestas, non fermentum nisl euismod.
+              Curabitur ac dapibus purus. Ut blandit tincidunt purus quis
+              suscipit. Sed porttitor ac nisl vel elementum. Sed non sem
+              consequat dui lobortis egestas sit amet et tortor. Nulla et neque
+              at ipsum blandit cursus. Aenean nec felis aliquam nisl lacinia
+              fringilla.
+            </p>
+          </div>
+          <div>
+            <button
+              className={twJoin(
+                "px-4 py-1",
+                "shadow-sm shadow-gray-200",
+                "border",
+                "rounded-md",
+              )}
+            >
+              ^80
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
